b066c7ef2b32cee4f55c7cf055a344efa4e5cf64
remusalex2 <remus.alex2@gmail.com>
2023-01-03 20:25:20 +0200

    added read / write functions for a matrix

--- MAP/Source.c
+++ MAP/Source.c
@@ -1,12 +1,48 @@
-#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
 
-void afisare()
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void citire(int a[][20], int m, int n)
 {
-	printf("test");
+	int i, j;
+	for (i = 0; i < m; i++) 
+	{
+		for (j = 0; j < n; j++) 
+		{
+			printf("a[%d][%d]=", i, j);
+			scanf("%d", &a[i][j]);
+		}
+	}
+}
+
+void afisare(int mat [][20], int m, int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			printf("%d ", mat[i][j]);
+		}
+
+		printf("\n");
+	}
 }
 
 int main()
 {
-	afisare();
+	int mat[20][20], m, n;
+
+	printf("m="); 
+	scanf("%d", &m);
+
+	printf("n="); 
+	scanf("%d", &n);
+
+
+	citire(mat, m, n);
+	afisare(mat, m, n);
 	return 0;
 }
